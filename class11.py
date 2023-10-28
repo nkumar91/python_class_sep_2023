f3865485ffce53cb297f6edcfaa26dc2d2528f55
Nishant <nknishant91@gmail.com>
2023-10-28 12:02:32 +0530

    function class

---
+++ class11.py
@@ -0,0 +1,39 @@
+# default arguments
+
+
+# def my(x = 2,y = 3):
+#     z=x+y
+#     print(z)
+
+# my()
+
+
+# arbitray arguments (*)
+# def sum(*x):
+#     print(type(x))
+#     s = 0
+#     for y in x:
+#         s+=y
+#     print(s)
+# sum(10,20,30)
+
+
+
+# key value arguments
+
+
+# def myFun(x,y,z):
+#     print(x,y,z)
+# myFun(y="hii",x = "hello",z=50)
+
+
+#**
+def myFun(**xyz):
+    print(type(xyz))
+    print(xyz['x'])
+myFun(x=5,y=2,z=3)
+
+
+
+
+
